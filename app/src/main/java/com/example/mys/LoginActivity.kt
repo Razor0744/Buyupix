d7389f372a50586c87dd8ccdb0323cfb2aa61791
Razor <sgavrilov387@gmail.com>
2021-12-04 15:05:35 +0300

    Регистрация и вход пользователя, но выдаёт текст ошибки если пользователь уже есть

---
+++ app/src/main/java/com/example/mys/LoginActivity.kt
@@ -0,0 +1,95 @@
+package com.example.mys
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        val emailEditText: EditText = findViewById(R.id.login)
+        val passwordEditText: EditText = findViewById(R.id.password)
+        val login = emailEditText.text
+        val password = passwordEditText.text
+        val buttonCreateOrLogin: Button = findViewById(R.id.LoginOrCreate)
+        buttonCreateOrLogin.setOnClickListener() {
+            createAccount(login.toString(), password.toString())
+            signIn(login.toString(), password.toString())
+        }
+    }
+
+    //public override fun onStart() {
+    //    super.onStart()
+    //    // Check if user is signed in (non-null) and update UI accordingly.
+   //     val currentUser = auth.currentUser
+    //    if (currentUser != null) {
+    //        reload();
+    //    }
+  //  }
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        applicationContext, "Введите почту",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        applicationContext, "Неверный логин или пароль",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun reload() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
