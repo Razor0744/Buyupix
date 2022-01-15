6d9f1efeb7511e374ac3dabbe0da971b9945841a
Razor <sgavrilov387@gmail.com>
2022-01-15 10:07:05 +0300

    RecyclerView with Firestore

--- app/src/main/java/com/example/mys/SearchActivity.kt
+++ app/src/main/java/com/example/mys/SearchActivity.kt
@@ -1,13 +1,11 @@
 package com.example.mys
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_calendar)
     }
 }
