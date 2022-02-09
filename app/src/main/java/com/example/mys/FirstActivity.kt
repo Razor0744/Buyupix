65fd9a0844f4eeaf3b335a056c0aeb678a5bf0f4
Razor <sgavrilov387@gmail.com>
2022-02-09 03:07:18 +0300

    full FirstActivity.kt

--- app/src/main/java/com/example/mys/FirstActivity.kt
+++ app/src/main/java/com/example/mys/FirstActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mys.databinding.ActivityFirstBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FirstActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding : ActivityFirstBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +25,21 @@ class FirstActivity : AppCompatActivity() {
         binding.signUp.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun reload() {
+        val intent = Intent(this, SubscriptionsActivity::class.java)
+        startActivity(intent)
     }
 }
