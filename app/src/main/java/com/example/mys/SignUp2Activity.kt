b7fbf63a905a4aac43442b9c6b9f33a7759e658b
Razor <sgavrilov387@gmail.com>
2022-02-15 09:45:49 +0300

    init verification

--- app/src/main/java/com/example/mys/SignUp2Activity.kt
+++ app/src/main/java/com/example/mys/SignUp2Activity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mys.databinding.ActivitySignUp2Binding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SignUp2Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUp2Binding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +25,18 @@ class SignUp2Activity : AppCompatActivity() {
             signUp3Activity.putExtra("Email", email)
             startActivity(signUp3Activity)
         }
+
+        sendEmailVerification()
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = FirebaseAuth.getInstance().currentUser
+        println(user.toString())
+        user!!.sendEmailVerification()
+            .addOnSuccessListener {
+               println("good")
+            }
+        // [END send_email_verification]
     }
 }
