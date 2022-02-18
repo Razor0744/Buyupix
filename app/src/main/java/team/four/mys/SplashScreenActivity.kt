767c1885d03c91446b95d725dcfa3904039697a9
Razor <sgavrilov387@gmail.com>
2022-02-18 12:44:22 +0300

    SplashScreenActivity.kt and refactoring

--- app/src/main/java/team/four/mys/SplashScreenActivity.kt
+++ app/src/main/java/team/four/mys/SplashScreenActivity.kt
@@ -4,26 +4,25 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
+import android.os.Looper
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val handler = Handler()
-
     private val runnable = Runnable {
         if (!isFinishing) {
-            startActivity(Intent(applicationContext, FirstActivity::class.java))
+            startActivity(Intent(this, FirstActivity::class.java))
             finish()
         }
     }
 
     override fun onResume() {
         super.onResume()
-        handler.postDelayed(runnable, 200)
+        Handler(Looper.getMainLooper()).postDelayed(runnable, 2000)
     }
 
     override fun onPause() {
         super.onPause()
-        handler.removeCallbacks(runnable)
+        Handler(Looper.getMainLooper()).removeCallbacks(runnable)
     }
 }
