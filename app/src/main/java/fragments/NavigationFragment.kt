8dfe742aa430a2670b65508fd4ecf50aff5dc7c8
Razor <sgavrilov387@gmail.com>
2022-06-14 16:06:29 +0300

    full navigation bar

--- app/src/main/java/fragments/NavigationFragment.kt
+++ app/src/main/java/fragments/NavigationFragment.kt
@@ -38,7 +38,7 @@ class NavigationFragment : Fragment() {
         val drawableHome = resources.getDrawable(R.drawable.ic_home_click, context?.theme)
         val drawableStatistics = resources.getDrawable(R.drawable.ic_statistics_click, context?.theme)
         val drawableSettings = resources.getDrawable(R.drawable.ic_settings_click, context?.theme)
-        val color = resources.getColor(R.color.blue_200, context?.theme)
+        val color = resources.getColor(R.color.textNavigationBar, context?.theme)
         val fontFamily = context?.let { ResourcesCompat.getFont(it, R.font.roboto_medium) }
 
         when (i) {
