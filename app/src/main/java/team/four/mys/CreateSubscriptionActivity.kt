a0c456f0db82c55c7be10c5b1afb0f911cb8ac0b
Razor <sgavrilov387@gmail.com>
2022-09-02 15:19:52 +0300

    button calendar

--- app/src/main/java/team/four/mys/CreateSubscriptionActivity.kt
+++ app/src/main/java/team/four/mys/CreateSubscriptionActivity.kt
@@ -38,7 +38,11 @@ class CreateSubscriptionActivity : AppCompatActivity() {
                         "description" to binding.description.text.trim().toString()
                     )
                 } else {
-                    Toast.makeText(this, "Бля, ты конечно приколист, а цену я выдумывать должен?", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this,
+                        "Бля, ты конечно приколист, а цену я выдумывать должен?",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             } else {
                 Toast.makeText(this, "Ты ебаны рот название напиши", Toast.LENGTH_LONG).show()
@@ -46,36 +50,34 @@ class CreateSubscriptionActivity : AppCompatActivity() {
         }
     }
 
-}
-
-private fun uid(): String {
-    // get UID
-    val user = FirebaseAuth.getInstance().currentUser
-    val uid = user?.uid
-    return uid.toString()
-}
-
-//url icons from storage firebase
-private fun icons(): String {
-    var url = ""
-    when (binding.nameActivity.text) {
-        "Spotify" -> {
-            url =
-                "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Spotify.png?alt=media&token=89cd172f-201d-4a5e-acc6-e0da3344c26e"
-        }
-        "VK Combo" -> {
-            url =
-                "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/VK%20Kombo.png?alt=media&token=edc2d633-0e2b-4610-9fc0-301552bc679b"
-        }
-        "Netflix" -> {
-            url =
-                "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Netfix.png?alt=media&token=769f754e-3a71-44c1-ac59-86e59c7ef412"
-        }
-        "Apple Music" -> {
-            url =
-                "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Apple%20Music.png?alt=media&token=c4b5b8d5-4af6-4095-a332-2375daa55b8d"
-        }
+    private fun uid(): String {
+        // get UID
+        val user = FirebaseAuth.getInstance().currentUser
+        val uid = user?.uid
+        return uid.toString()
     }
-    return url
-}
+
+    //url icons from storage firebase
+    private fun icons(): String {
+        var url = ""
+        when (binding.nameActivity.text) {
+            "Spotify" -> {
+                url =
+                    "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Spotify.png?alt=media&token=89cd172f-201d-4a5e-acc6-e0da3344c26e"
+            }
+            "VK Combo" -> {
+                url =
+                    "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/VK%20Kombo.png?alt=media&token=edc2d633-0e2b-4610-9fc0-301552bc679b"
+            }
+            "Netflix" -> {
+                url =
+                    "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Netfix.png?alt=media&token=769f754e-3a71-44c1-ac59-86e59c7ef412"
+            }
+            "Apple Music" -> {
+                url =
+                    "https://firebasestorage.googleapis.com/v0/b/my-subscriptions-96306.appspot.com/o/Apple%20Music.png?alt=media&token=c4b5b8d5-4af6-4095-a332-2375daa55b8d"
+            }
+        }
+        return url
+    }
 }
