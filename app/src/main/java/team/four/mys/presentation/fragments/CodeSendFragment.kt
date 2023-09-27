17c790b9ef454e37f2b24b084dd657893127a7a9
razor <sgavrilov387@gmail.com>
2023-09-27 19:19:10 -0400

    refactor icon and code send fragment new design

--- app/src/main/java/team/four/mys/presentation/fragments/CodeSendFragment.kt
+++ app/src/main/java/team/four/mys/presentation/fragments/CodeSendFragment.kt
@@ -1,6 +1,8 @@
 package team.four.mys.presentation.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth
 import team.four.mys.R
 import team.four.mys.databinding.FragmentCodeSendBinding
 import team.four.mys.presentation.activity.MainActivity
+
 
 class CodeSendFragment : Fragment() {
 
@@ -32,11 +35,17 @@ class CodeSendFragment : Fragment() {
         binding.code1.requestFocus()
 
         binding.buttonArrowLeft.setOnClickListener {
-            findNavController().navigate(R.id.login_fragment)
+            findNavController().popBackStack()
+        }
+
+        binding.textViewDoNotHaveCode.setOnClickListener {
+            binding.textViewResendCode.visibility = View.VISIBLE
+            binding.textViewDoNotHaveCode.visibility = View.GONE
+            timer()
         }
 
         textWatcher()
-
+        editorAction()
 
         return binding.root
     }
@@ -55,29 +64,21 @@ class CodeSendFragment : Fragment() {
         binding.code2.doAfterTextChanged {
             if (it?.length == 1) {
                 binding.code3.requestFocus()
-            } else {
-                binding.code1.requestFocus()
             }
         }
         binding.code3.doAfterTextChanged {
             if (it?.length == 1) {
                 binding.code4.requestFocus()
-            } else {
-                binding.code2.requestFocus()
             }
         }
         binding.code4.doAfterTextChanged {
             if (it?.length == 1) {
                 binding.code5.requestFocus()
-            } else {
-                binding.code3.requestFocus()
             }
         }
         binding.code5.doAfterTextChanged {
             if (it?.length == 1) {
                 binding.code6.requestFocus()
-            } else {
-                binding.code4.requestFocus()
             }
         }
         binding.code6.doAfterTextChanged {
@@ -88,6 +89,71 @@ class CodeSendFragment : Fragment() {
                     .toString()
                 (activity as MainActivity).verifyPhoneNumberWithCode(code = text)
             }
+        }
+    }
+
+    private fun timer() {
+        var counter = 30
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.textViewResendCode.text =
+                    getString(
+                        R.string.resend_code_code_send_fragment,
+                        counter.toString()
+                    )
+                counter--
+            }
+
+            override fun onFinish() {
+                binding.textViewResendCode.visibility = View.GONE
+                binding.textViewDoNotHaveCode.visibility = View.VISIBLE
+                binding.textViewDoNotHaveCode.setOnClickListener {
+                    (activity as MainActivity).resendVerificationCode()
+                }
+            }
+        }.start()
+    }
+
+    private fun editorAction() {
+        binding.code2.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL) {
+                if (binding.code2.text.trim().toString().isEmpty()) {
+                    binding.code1.requestFocus()
+                }
+            }
+            false
+        }
+        binding.code3.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL) {
+                if (binding.code3.text.trim().toString().isEmpty()) {
+                    binding.code2.requestFocus()
+                }
+            }
+            false
+        }
+        binding.code4.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL) {
+                if (binding.code4.text.trim().toString().isEmpty()) {
+                    binding.code3.requestFocus()
+                }
+            }
+            false
+        }
+        binding.code5.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL) {
+                if (binding.code5.text.trim().toString().isEmpty()) {
+                    binding.code4.requestFocus()
+                }
+            }
+            false
+        }
+        binding.code6.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL) {
+                if (binding.code6.text.trim().toString().isEmpty()) {
+                    binding.code5.requestFocus()
+                }
+            }
+            false
         }
     }
 }
