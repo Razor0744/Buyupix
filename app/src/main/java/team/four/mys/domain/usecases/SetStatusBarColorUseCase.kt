16135f924365f81c2b8033db310124c2da609a08
siarheihaurylau <sgavrilov387@gmail.com>
2023-12-19 13:17:08 +0300

    change deprecated status bar color icons

--- app/src/main/java/team/four/mys/domain/usecases/SetStatusBarColorUseCase.kt
+++ app/src/main/java/team/four/mys/domain/usecases/SetStatusBarColorUseCase.kt
@@ -2,7 +2,7 @@ package team.four.mys.domain.usecases
 
 import android.app.Activity
 import android.content.res.Configuration
-import android.view.View
+import androidx.core.view.WindowInsetsControllerCompat
 import team.four.mys.domain.models.SettingsPreferencesParam
 import javax.inject.Inject
 
@@ -15,28 +15,52 @@ class SetStatusBarColorUseCase @Inject constructor(
         when (getSettingsUseCase.execute(SettingsPreferencesParam(key = "DarkMode")).value) {
             "System Theme" -> {
                 when (activity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                    Configuration.UI_MODE_NIGHT_YES -> activity.window.decorView.systemUiVisibility =
-                        (View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        WindowInsetsControllerCompat(
+                            activity.window,
+                            activity.window.decorView
+                        ).isAppearanceLightStatusBars = false
+                    }
 
-                    Configuration.UI_MODE_NIGHT_NO -> activity.window.decorView.systemUiVisibility =
-                        (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        WindowInsetsControllerCompat(
+                            activity.window,
+                            activity.window.decorView
+                        ).isAppearanceLightStatusBars = true
+                    }
                 }
             }
 
             "Dark Theme"
-            -> activity.window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            -> {
+                WindowInsetsControllerCompat(
+                    activity.window,
+                    activity.window.decorView
+                ).isAppearanceLightStatusBars = false
+            }
 
-            "Light Theme" -> activity.window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            "Light Theme" -> {
+                WindowInsetsControllerCompat(
+                    activity.window,
+                    activity.window.decorView
+                ).isAppearanceLightStatusBars = true
+            }
 
             else -> {
                 when (activity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                    Configuration.UI_MODE_NIGHT_YES -> activity.window.decorView.systemUiVisibility =
-                        (View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        WindowInsetsControllerCompat(
+                            activity.window,
+                            activity.window.decorView
+                        ).isAppearanceLightStatusBars = false
+                    }
 
-                    Configuration.UI_MODE_NIGHT_NO -> activity.window.decorView.systemUiVisibility =
-                        (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+                    Configuration.UI_MODE_NIGHT_NO -> {
+                        WindowInsetsControllerCompat(
+                            activity.window,
+                            activity.window.decorView
+                        ).isAppearanceLightStatusBars = true
+                    }
                 }
             }
         }
